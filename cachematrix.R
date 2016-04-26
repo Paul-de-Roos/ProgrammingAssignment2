da32e25bfb6258ed493b350cc269af19e3d26940
Paul-de-Roos <paul.de.roos@isatis-business-solutions.nl>
2016-04-26 13:22:31 +0200

    Added makeCacheMatrix and cacheSolve functions
    Filled out the template with the proper syntax.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## this function makes a list of 4 functions which can be assigned to a vector and subsequently
+## 'called' by the cacheSolve function
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## this funtion checks if its argument is stored with the appropiate functions assigned to it.
+## If so, it will retrieve the inverse matrix from cache. If not, it will calculate the inverse matrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
